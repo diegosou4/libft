0607ba6aeb5364422c51fc6e6457a8dad3bc9b83
Diego Moreira Sardinha De Souza <diegmore@c2r8s3.42lisboa.com>
2023-10-03 11:14:25 +0100

    Done

--- src/ft_bzero.c
+++ src/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: diegmore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 10:35:50 by diegmore          #+#    #+#             */
-/*   Updated: 2023/10/03 10:40:01 by diegmore         ###   ########.fr       */
+/*   Updated: 2023/10/03 10:43:59 by diegmore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_bzero(void *s, size_t n)
 	char	b;
 
 	p_s = s;
-	b = '\0';
+	b = 0;
 	while (n--)
 		*(unsigned char *)s++ = (unsigned char *)b;
 	return (p_s);
